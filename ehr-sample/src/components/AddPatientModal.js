71956e4d53632bff0f3c36c7d1d90c63cfb3ba92
Andrew Hall <andrew.b.hall@gmail.com>
2024-07-11 22:52:47 -0700

    made styling more responsive

--- ehr-sample/src/components/AddPatientModal.js
+++ ehr-sample/src/components/AddPatientModal.js
@@ -25,85 +25,89 @@ const AddPatientModal = ({ onSave, onCancel }) => {
 
     return (
         <div className="modal">
-            <h2>Create New Patient</h2>
-            <div className="row">
-                <div className="inputGroup">
-                    <label>First Name</label>
-                    <input type="text" value={newPatient.firstName} onChange={e => handleChange('firstName', e.target.value)} />
-                </div>
-                <div className="inputGroup">
-                    <label>Middle Name</label>
-                    <input type="text" value={newPatient.middleName} onChange={e => handleChange('middleName', e.target.value)} />
-                </div>
-                <div className="inputGroup">
-                    <label>Last Name</label>
-                    <input type="text" value={newPatient.lastName} onChange={e => handleChange('lastName', e.target.value)} />
-                </div>
+            <div className="modalHeader">
+                <h2>Create New Patient</h2>
             </div>
-            <div className="row">
-                <div className="inputGroup">
-                    <label>Date of Birth</label>
-                    <input type="date" value={newPatient.dob} onChange={e => handleChange('dob', e.target.value)} />
+            <div className='modalBody'>
+                <div className="row">
+                    <div className="inputGroup">
+                        <label>First Name</label>
+                        <input type="text" value={newPatient.firstName} onChange={e => handleChange('firstName', e.target.value)} />
+                    </div>
+                    <div className="inputGroup">
+                        <label>Middle Name</label>
+                        <input type="text" value={newPatient.middleName} onChange={e => handleChange('middleName', e.target.value)} />
+                    </div>
+                    <div className="inputGroup">
+                        <label>Last Name</label>
+                        <input type="text" value={newPatient.lastName} onChange={e => handleChange('lastName', e.target.value)} />
+                    </div>
                 </div>
-                <div className="inputGroup">
-                    <label>Gender with Insurance</label>
-                    <select
-                        value={newPatient.genderWithInsurance}
-                        onChange={e => handleChange('genderWithInsurance', e.target.value)}
-                    >
-                        <option value="">Select Gender</option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="Nonbinary">Nonbinary</option>
-                    </select>
+                <div className="row">
+                    <div className="inputGroup">
+                        <label>Date of Birth</label>
+                        <input type="date" value={newPatient.dob} onChange={e => handleChange('dob', e.target.value)} />
+                    </div>
+                    <div className="inputGroup">
+                        <label>Gender with Insurance</label>
+                        <select
+                            value={newPatient.genderWithInsurance}
+                            onChange={e => handleChange('genderWithInsurance', e.target.value)}
+                        >
+                            <option value="">Select Gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Nonbinary">Nonbinary</option>
+                        </select>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="inputGroup">
-                    <label>Phone Number</label>
-                    <input type="text" value={newPatient.phoneNumber} onChange={e => handleChange('phoneNumber', e.target.value)} />
+                <div className="row">
+                    <div className="inputGroup">
+                        <label>Phone Number</label>
+                        <input type="text" value={newPatient.phoneNumber} onChange={e => handleChange('phoneNumber', e.target.value)} />
+                    </div>
+                    <div className="inputGroup">
+                        <label>Email</label>
+                        <input type="email" value={newPatient.email} onChange={e => handleChange('email', e.target.value)} />
+                    </div>
                 </div>
-                <div className="inputGroup">
-                    <label>Email</label>
-                    <input type="email" value={newPatient.email} onChange={e => handleChange('email', e.target.value)} />
+                <div className="row">
+                    <div className="inputGroup">
+                        <label>Address</label>
+                        <input type="text" value={newPatient.address} onChange={e => handleChange('address', e.target.value)} />
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="inputGroup">
-                    <label>Address</label>
-                    <input type="text" value={newPatient.address} onChange={e => handleChange('address', e.target.value)} />
+                <div className="row">
+                    <div className="inputGroup">
+                        <label>City</label>
+                        <input type="text" value={newPatient.city} onChange={e => handleChange('city', e.target.value)} />
+                    </div>
+                    <div className="inputGroup">
+                        <label>State</label>
+                        <input type="text" value={newPatient.state} onChange={e => handleChange('state', e.target.value)} />
+                    </div>
+                    <div className="inputGroup">
+                        <label>Zip</label>
+                        <input type="text" value={newPatient.zip} onChange={e => handleChange('zip', e.target.value)} />
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="inputGroup">
-                    <label>City</label>
-                    <input type="text" value={newPatient.city} onChange={e => handleChange('city', e.target.value)} />
+                <div className="row">
+                    <div className="inputGroup">
+                        <label>Referring Physician</label>
+                        <input type="text" value={newPatient.referringPhysician} onChange={e => handleChange('referringPhysician', e.target.value)} />
+                    </div>
                 </div>
-                <div className="inputGroup">
-                    <label>State</label>
-                    <input type="text" value={newPatient.state} onChange={e => handleChange('state', e.target.value)} />
+                <div className="row">
+                    <div className="inputGroup">
+                        <label>Diagnosis</label>
+                        <input type="text" value={newPatient.diagnosis} onChange={e => handleChange('diagnosis', e.target.value)} />
+                    </div>
                 </div>
-                <div className="inputGroup">
-                    <label>Zip</label>
-                    <input type="text" value={newPatient.zip} onChange={e => handleChange('zip', e.target.value)} />
-                </div>
-            </div>
-            <div className="row">
-                <div className="inputGroup">
-                    <label>Referring Physician</label>
-                    <input type="text" value={newPatient.referringPhysician} onChange={e => handleChange('referringPhysician', e.target.value)} />
-                </div>
-            </div>
-            <div className="row">
-                <div className="inputGroup">
-                    <label>Diagnosis</label>
-                    <input type="text" value={newPatient.diagnosis} onChange={e => handleChange('diagnosis', e.target.value)} />
-                </div>
-            </div>
-            <div className="row">
-                <div className="textareaGroup">
-                    <label>Treatment Notes</label>
-                    <textarea value={newPatient.treatmentNotes} onChange={e => handleChange('treatmentNotes', e.target.value)} rows="5"></textarea>
+                <div className="row">
+                    <div className="textareaGroup">
+                        <label>Treatment Notes</label>
+                        <textarea value={newPatient.treatmentNotes} onChange={e => handleChange('treatmentNotes', e.target.value)} rows="3"></textarea>
+                    </div>
                 </div>
             </div>
             <div className="buttons">
--- ehr-sample/src/components/Content.js
+++ ehr-sample/src/components/Content.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Patients from './Patients';
 
 const Content = () => {
